2308d20eed600af1feab5f8fce1d13bfc5314df0
Suraj Kadam <kadamsuraj290@gmail.com>
2026-08-26 11:23:21 +0530

    26/8/25

---
+++ core python/Data_sturctures/String Demo/intro.py
@@ -0,0 +1,26 @@
+#1. '',"",''' ''',""" """
+
+str ='FirstBit solutions'
+
+
+str ="FirstBit solutions"
+
+
+str =''' This is first line.
+This is second line.'''
+
+
+str ="""This is first line.
+This is second line.324321"""
+
+
+#2. Set of character(alpha,numeric,symbolic,space)
+
+
+#3.Orderd
+
+
+#4.Immutable
+
+
+#5.Duplication allowed
---
+++ core python/Data_sturctures/Tuple demo/intro.py
@@ -0,0 +1,17 @@
+#1. ()
+tu = (10,20,30,40)
+#2. heterogenues
+#tu = (10,'abc',3.14,10)
+#3.Orderd
+
+#4. Immutable
+#tu =[0] =7
+
+#5. Duplication allowd
+
+#6.Faster than list
+print(type(tu))
+print(tu)
+
+import sys
+print(sys.getsizeof(tu))
